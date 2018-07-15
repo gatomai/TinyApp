0bac300cec9291706c5facd5c0efea7f34589667
Sai Gautam <sai.gautam@gmail.com>
2018-07-15 15:37:27 +0000

    Fixed bug related to addition of new url for new and existing user

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # TinyApp
+
+TinyApp is a lightweight clone of bit.ly that is a full stack web application build on Node and Express. 
+Its primary purpose is to shorten URLs.
+
+## Dependencies
+
+-Node.js
+-Express
+-ejs
+-bcryptjs
+-body-parser
+-cookie-session
+
+## Getting Started
+
+- Install all dependencies (using the npm install command).
+- Run the development web server using 'node express_server.js' command.
+
+## Final Product
+
+!["Screenshot of URLs"](https://github.com/gatomai/tinyapp/docs/URL-Dashboard.png)
+!["Screenshot of register"](https://github.com/gatomai/tinyapp/docs/Register-New_user.png)
+!["Screenshot of Add New URL Page"](https://github.com/gatomai/tinyapp/docs/Add-New-URL.png)
+!["Screenshot of Add New URL Page"](https://github.com/gatomai/tinyapp/docs/Add-New-URL.png)
+!["Screenshot of Before Update"](https://github.com/gatomai/tinyapp/docs/Pre-UPDATE.png)
+!["Screenshot of After Update"](https://github.com/gatomai/tinyapp/docs/Post-UPDATE.png)
+!["Screenshot of Before Delete"](https://github.com/gatomai/tinyapp/docs/PreDelete-Dashboard.png)
+!["Screenshot of After Delete"](https://github.com/gatomai/tinyapp/docs/Post-Delete.png)
+
+
+
+
+
+
+
+
--- express_server.js
+++ express_server.js
@@ -1,23 +1,25 @@
+//Require declarations and their instantiations
+
 var express = require("express");
-// var cookie = require("cookie-parser");
-var session = require('cookie-session')
-const bcrypt = require('bcryptjs');
-// var reload = require("reload")
 var app = express();
-var PORT = 8080; // default port 8080
 
-const bodyParser = require("body-parser");
-
-// app.use(session());
+var session = require('cookie-session')
 app.use(session({
   name: 'session',
   keys: ['Rain-in-Spain', 'sdvsdf'],
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
+
+const bcrypt = require('bcryptjs');
+
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+// MIDDLEWARE DEBUGGER
 // app.use(function (req, res, next) {
 //   console.log(req.method + ": " +req.path);
 //   console.log(req.cookies);
@@ -29,6 +31,8 @@ app.set("view engine", "ejs");
 //   next();
 // });
 
+//Declarations of User->URLDatabase
+//Known Bug - Express is unable to read the first record properly, so a dummy record is inserted as the first.
 const urlDatabase = {
   "ZZZZZ": {
     "b23Vy9": "http://hereistoday.com/",
@@ -48,28 +52,30 @@ const urlDatabase = {
   }
 };
 
-//passwords: Stored for Testing ONLY
+//passwords: Note to Mentor: Stored for your Testing ONLY
 //"user2@example.com" "purple-monkey-dinosaur"
 //"a@a.com" "dishwasher-funk"
 
-
+//Declarations of User->Logins
 const users = {
   "AAAAA": {
     id: "AAAAA",
     email: "user@example.com",
-    password: "$2a$10$HLujcMUDftWaHYhtv6PnJ.jvrP/zlz9COe/Wh3x9GLOzbnhSa6LlO"
+    password: bcrypt.hashSync("purple-monkey-dinosaur", 10)
   },
   "BBBBB": {
     id: "BBBBB",
     email: "user2@example.com",
-    password: "$2a$10$fiDwBuuSE8/TQAQgam3Ee.j1gaDY3Ank2nBiFRMDC1glmQimmfXni"
+    password: bcrypt.hashSync("dishwasher-funk", 10)
   },
   "abcde": {
     id: "abcde",
     email: "a@a.com",
-    password: "$2a$10$.Q7n9m1OxPJkrol6TnYlY.IEkBoRPncr.Hl/xhygYdjGMC/npYXXi"
+    password: bcrypt.hashSync("123", 10)
   }
 };
+
+// Routes and Endpoints
 
 app.get("/", (req, res) => {
   let templateVars = {
@@ -81,28 +87,19 @@ app.get("/", (req, res) => {
 
 
 app.get("/u/:id", (req, res) => {
-  console.log(req.params,'g');
-  // console.log(req.params.id,'g');
-  // let longURL = urlForShortIs(req.params.id);
   let longURL = urlDatabase[req.session.user_id][req.params.id];
-  console.log(longURL,'longURL in /u/:id');
   res.redirect(longURL);
 });
 
 app.get("/register", (req, res) => {
-  console.log(res.body, 'b');
   let templateVars = {
     user_id: req.session.user_id,
     user: users[req.session.user_id]
   }
-  console.log(req.session.user_id,'d');
-  console.log(templateVars,'e');
   res.render("urls_signin", templateVars);
 });
 
 app.post("/register", (req, res) => {
-  console.log(req.body, 'c');
-
   if (req.body.email.length === 0 || req.body.password.length === 0) {
     res.status(400);
     res.send('Email and/or Password cannot be blank');
@@ -118,7 +115,6 @@ app.post("/register", (req, res) => {
   if (req.body.email.length > 0 && req.body.password.length > 0) {
     let user_id = generateRandomString();
     users[user_id] = { id: user_id, email: req.body.email, password: bcrypt.hashSync(req.body.password) };
-    console.log(users[user_id], 'c');
     req.session.user_id = users[user_id].id;
     res.redirect('/urls');
   }
@@ -144,7 +140,6 @@ app.get("/urls", (req, res) => {
     user: users[req.session.user_id],
     urls: urlDatabase[req.session.user_id]
   };
-  console.log(templateVars,'/urls');
   res.render("urls_index", templateVars);
 });
 
@@ -153,48 +148,38 @@ app.get("/urls/new", (req, res) => {
     user_id: req.session.user_id,
     user: users[req.session.user_id]
   }
-  console.log(templateVars,'/urls/new')
   res.render("urls_new", templateVars);
-  // res.redirect('/urls');
+  res.redirect('/urls');
 });
 
 app.get("/urls/:id", (req, res) => {
   let param = req.params.id;
-  console.log(req.params.id,'f');
-  console.log(req.session.user_id,'Session-user_id');  
-  console.log(',XXXX',urlIsForUser(req.params.id));
   let templateVars = {
-    // user_id: urlIsForUser(req.params.id),
     user_id: req.session.user_id,
     user: users[req.session.user_id],
     shortURL: req.params.id,
     urls: urlDatabase,
-    // longURL : urlDatabase[req.session.user_id].param,
     urls: urlDatabase[req.session.user_id]
-    // urls: Object.values(urlDatabase.req.session.user_id)
     };
-  console.log(templateVars)
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  // console.log(req.body, 'x');  // debug statement to see POST parameters
-  // res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
   let templateVars = {
     user_id: req.session.user_id
   }
   
-  let rando = generateRandomString();
-  // console.log(rando,'Random String');
+  let shortURL = generateRandomString();
   let session_id = req.session.user_id;
-  // console.log(session_id,'SessionID');
-  // urlDatabase[generateRandomString()] = req.body.longURL;
-  // console.log(urlDatabase[req.session.user_id],'Before');
-  // urlDatabase[req.session.user_id][rando] = req.body.longURL;
-  urlDatabase[session_id] = {[rando]: req.body.longURL};
-  // console.log(urlDatabase[req.session.user_id],'After');
+  let longURL = req.body.longURL;
+  if (checkUserURLDb(session_id)) {
+    urlDatabase[session_id][shortURL] = longURL;
+  } else {
+    urlDatabase[session_id] = {
+      [shortURL]: longURL
+    };    
+  }  
   res.set(req.body.longURL);
-  // res.send(req.body.longURL);
   res.redirect('/urls');
 });
 
@@ -202,26 +187,19 @@ app.post("/urls/:id/delete", (req, res) => {
   let templateVars = {
     user_id: req.session.user_id
   }
-  console.log(req.body, 'x');  // debug statement to see POST parameters
   delete urlDatabase[req.session.user_id][req.params.id];
   res.redirect('/urls');
 });
 
 app.post("/urls/:id/update", (req, res) => {
-  console.log(req.body, 'y');  // debug statement to see POST parameters
   let templateVars = {
     user_id: req.session.user_id
   }
-  // console.log(req.body.longURL);
   urlDatabase[req.session.user_id][req.params.id] = req.body.longURL;
   res.redirect('/urls');
 });
 
 app.post("/login", (req, res) => {
-  console.log(req.body, 'z');  // debug statement to see POST parameters
-  // console.log(req.body.longURL);
-  // urlDatabase[req.params.id] = req.body.longURL;
-  // res.cookie(req.body.username, req.body.username, { domain: 'localhost', path: '/login', secure: true });
   let templateVars = {
     user_id: req.session.user_id
   }
@@ -233,21 +211,14 @@ app.post("/login", (req, res) => {
 
   for (let element in users) {
     if (req.body.email === users[element].email) {
-      // if (req.body.password === users[element].password) {
-        // console.log(users[element].password);
-        // console.log(bcrypt.hashSync(req.body.password, 10));
         if (bcrypt.compareSync(req.body.password, users[element].password)) {
-        // res.send('Email and password checks out');
-        // res.cookie('userid', users[element].id);        
         req.session.user_id = users[element].id;
-        console.log(req.session.user_id,'user_id')
         res.redirect('/urls');
       }
     }
   }
 
   for (let element in users) {
-    console.log(users[element].email,'User Email');
     if (req.body.email !== users[element].email && (!(bcrypt.compareSync(req.body.password,users[element].password)))) {
         res.status(403);
         res.send('Email and/or Password is wrong');
@@ -256,7 +227,6 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-  console.log(res.body, 'a');
   res.clearCookie('user_id');
   res.redirect('/login');
 });
@@ -287,6 +257,8 @@ function makeid() {
   return text;
 }
 
+//Function to return the User associated with the Short URL
+//Deprecated due to Cookies functionality
   function urlIsForUser(id) {
     for(element in urlDatabase) {
       if (urlDatabase[element] = id)
@@ -294,13 +266,10 @@ function makeid() {
     }
   }
 
-  function urlForShortIs(turl) {
-    console.log(turl,'Within urlForShortIs');
-    for(element in urlDatabase) {        
-        let lurl = (urlDatabase[element][turl]);
-        // console.log(element,urlDatabase[element][turl],turl)
-        if (lurl !== undefined) {
-            return "www.lg.com";
-        }
+  function checkUserURLDb(user_in) {
+    for(user in urlDatabase) {
+      if (user === user_in)
+      return true;
     }
+    return false;
   }
